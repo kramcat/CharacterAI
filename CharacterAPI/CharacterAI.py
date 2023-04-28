7844f5d68df8f796aba7589c7901de82cf3ed84d
Mark <130257905+kramcat@users.noreply.github.com>
2023-04-28 20:39:38 +0300

    Add files via upload

---
+++ CharacterAPI/CharacterAI.py
@@ -0,0 +1,128 @@
+from typing import Dict
+import json
+import time
+
+from playwright.sync_api import sync_playwright
+
+from errors import *
+
+__all__ = ['pyCAI']
+
+text = []
+page = None
+
+class pyCAI:
+    def __init__(self, token: str):
+        global page
+
+        self.token = token
+        self.browser = sync_playwright().start().firefox.launch(headless=True)
+        self.context = self.browser.new_context(extra_http_headers={"Authorization": f"Token {self.token}"})
+        page = self.context.new_page()
+
+        self.user = self.user()
+        self.character = self.character()
+        self.chat = self.chat()
+
+    # [FOR LIBRARY] Convert site to json
+    def GetResponce(link: str) -> Dict[str, str]:
+        pyCAI.goto(f'https://beta.character.ai/{link}/')
+        data = json.loads(page.locator('pre').inner_text())
+        
+        return data
+
+    # [FOR LIBRARY] Checking the page if it runs for the first time
+    def goto(link: str):
+        page.goto(link)
+
+        if page.title() != 'Waiting Room powered by Cloudflare':
+            if page.get_by_role("button", name="Accept").is_visible():
+                page.get_by_role("button", name="Accept").click()
+
+                return page
+        
+        else: raise errors.NoResponse('The Site is Overloaded')
+
+    class user:
+        """
+        Just a responces from site for user info
+
+        user.info()
+        """
+        def info(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/user')
+
+        def posts(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/posts/user/?scope=user&page=1&posts_to_load=5')
+
+        def followers(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/user/followers')
+
+        def following(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/user/following')
+
+    class character:
+        """
+        Just a responces from site for characters
+
+        character.trending()
+        character.get_info('CHAR')
+        """
+        def trending(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/characters/trending')
+        
+        def recommended(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/characters/recommended')
+
+        def categories(self) -> Dict[str, str]:
+            return pyCAI.GetResponce(link='chat/character/categories')
+
+        def get_info(self, char: str) -> Dict[str, str]:
+            try: return pyCAI.GetResponce(link=f'chat/character/info-cached/{char}')
+            except: raise errors.CharNotFound('Wrong Char')
+
+    class chat:
+        def get_history(self, char: str) -> Dict[str, str]:
+            """
+            Getting character chat history, return json responce
+
+            The post method doesn't work, so i get this from page.on('responce')
+
+            chat.get_history('CHAR')
+            """
+            page.on("response", lambda response: text.append(response.text()) if response.url.startswith('https://beta.character.ai/chat/history/msgs/user/') else None)
+            pyCAI.goto(f'https://beta.character.ai/chat?char={char}')
+            page.wait_for_selector('.msg.char-msg').is_visible()
+
+            return json.loads(text[0])
+
+        def send_message(self, char: str, message: str) -> Dict[str, str]:
+            """
+            Sending a message
+
+            chat.send_message('CHAR', 'MESSAGE')
+            """
+            pyCAI.goto(f'https://beta.character.ai/chat?char={char}')
+
+            time.sleep(1)
+            page.get_by_placeholder("Type a message").fill(message)
+            time.sleep(1)
+            page.get_by_role("button", name="Submit Message").click()
+            time.sleep(1)
+
+            page.wait_for_selector('.swiper-button-next', timeout=0).is_visible()
+            div = page.query_selector('div.markdown-wrapper.markdown-wrapper-last-msg.swiper-no-swiping')
+
+            return div.inner_text().replace('\n\n\n\n\n', '\n\n')
+
+        def new_chat(self, *, char: str) -> bool:
+            """
+            Starting new chat, return True when done
+
+            chat.new_chat('CHAR')
+            """
+            pyCAI.goto(f'https://beta.character.ai/chat?char={char}')
+            page.wait_for_selector('div.col-auto.px-2.dropdown').click()
+            page.locator('"Save and Start New Chat"').click()
+
+            return True
---
+++ CharacterAPI/__init__.py
@@ -0,0 +1,3 @@
+from .CharacterAPI import CharacterAI
+
+__all__ = ['CharacterAI']
