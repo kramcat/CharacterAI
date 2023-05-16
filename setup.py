0f23c8646bed5d6778f09ed23d08d016b5ae18bf
Mark <130257905+kramcat@users.noreply.github.com>
2023-05-16 23:17:37 +0300

    new v0.4.0

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open('README.md') as readme:
-    long_description = readme.read()
+with open('README.md', encoding='utf8') as f:
+    long_description = f.read()
 
 setup(
-    name='characterai'
-    version='0.2.7',
+    name='characterai',
+    version='0.4.1',
     author='kramcat',
     description='An unofficial API for character.ai for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/kramcat/CharacterAI',
-    packages=['characterai'],
-    install_requires=['playwright==1.32.1'],
+    url='https://github.com/kramcat/characterai',
+    packages=find_packages(),
+    install_requires=["playwright>=1.32.1"],
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
