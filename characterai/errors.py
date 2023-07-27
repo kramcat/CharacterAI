93afdffc7f995764f4010c5c09b8287ca17cdb75
SeoulSKY <swkim7519@protonmail.ch>
2023-07-26 18:44:20 -0600

    Fix AttributeError when the server is overloaded

--- characterai/errors.py
+++ characterai/errors.py
@@ -1,20 +1,26 @@
 class PyCAIError(Exception):
     pass
 
+
 class ServerError(PyCAIError):
     pass
+
 
 class FilterError(PyCAIError):
     pass
 
+
 class NotFoundError(PyCAIError):
     pass
+
 
 class LabelError(PyCAIError):
     pass
 
+
 class AuthError(PyCAIError):
     pass
+
 
 class NoResponseError(PyCAIError):
     pass
