e78a22a8a3c34454beee904b055c4af245443955
Mark <130257905+kramcat@users.noreply.github.com>
2023-10-05 23:39:27 +0300

    new v0.8.0

--- characterai/errors.py
+++ characterai/errors.py
@@ -4,25 +4,10 @@ class PyCAIError(Exception):
 class ServerError(PyCAIError):
     pass
 
-class FilterError(PyCAIError):
-    pass
-
-class NoChunksError(PyCAIError):
-    pass
-
-class NotFoundError(PyCAIError):
-    pass
-
 class LabelError(PyCAIError):
     pass
 
 class AuthError(PyCAIError):
-    pass
-
-class NoResponse(PyCAIError):
-    pass
-
-class UnknownError(PyCAIError):
     pass
 
 class PostTypeError(PyCAIError):
