b7d9e13a616ecd8cdca5faac6e37a29397455bda
Mark <130257905+kramcat@users.noreply.github.com>
2023-10-05 23:41:19 +0300

    new v0.8.0

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', encoding='utf8') as f:
 
 setup(
     name='characterai',
-    version='0.7.0',
+    version='0.8.0',
     author='kramcat',
     description='An unofficial API for character.ai for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/kramcat/characterai',
     packages=find_packages(),
-    install_requires=["playwright>=1.32.1"],
+    install_requires=["tls-client>=0.2.2"],
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
