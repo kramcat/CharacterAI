0dd35a58142da78fec3ceda6345a7f31850732a9
talwll <115516687+talwll@users.noreply.github.com>
2023-11-11 18:19:54 +0000

    character.ai flask

--- pythonCode/app.py
+++ pythonCode/app.py
@@ -1,27 +1,26 @@
 from flask import Flask, request, jsonify
 from characterai import PyCAI
 import requests
+#from characterai import main
 app = Flask(__name__)
 # Initialize the Character.ai client
-character_ai_client = PyCAI('a5c2f69274c87103925b32bd67ed45cff0b15957')
+client = PyCAI('a5c2f69274c87103925b32bd67ed45cff0b15957')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-@app.route('/get_text', methods=['POST'])
-def get_text():
-    # Handle Character.ai API request here
-    # Return the obtained text as JSON
-    pass
+
 @app.route('/start_chat', methods=['POST'])
+
 def start_chat():
     data = request.json
     char = data.get('char', '').lower()
+    print("Received data:", data)
 
     if char == 'father':
         char = 'uMnXQwm3ii7GGQUUExv4yfu4v_2_wYec4u-7K3H6Mbw'
 
-    chat = character_ai_client.chat.get_chat(char)
+    chat = client.chat.get_chat(char)
     participants = chat['participants']
 
     if not participants[0]['is_human']:
@@ -31,6 +30,17 @@ def start_chat():
 
     return jsonify({'tgt': tgt, 'chat_external_id': chat['external_id']})
 
+@app.route('/get_text', methods=['POST'])
+def get_text():
+    # Handle Character.ai API request here
+    data = request.json
+    char = data.get('char', '').lower()
+    # Get the obtained text from the Character.ai API response
+    text = client.chat.get_chat(char)['latest_message']['text']
+
+    # Return the obtained text as JSON
+    return jsonify({'text': text})
+
 @app.route('/send_message', methods=['POST'])
 def send_message():
     data = request.json
@@ -38,15 +48,12 @@ def send_message():
     tgt = data.get('tgt')
     message = data.get('message')
 
-    data = character_ai_client.chat.send_message(chat_external_id, tgt, message)
+    data = client.chat.send_message(chat_external_id, tgt, message)
 
     name = data['src_char']['participant']['name']
     text = data['replies'][0]['text']
 
     return jsonify({'name': name, 'text': text})
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 @app.route('/generate_video', methods=['POST'])
@@ -60,3 +67,4 @@ if __name__ == '__main__':
     # Example command: gunicorn -w 4 -b 0.0.0.0:5000 your_app_module:app
     import os
     os.system('gunicorn -w 4 -b 0.0.0.0:5000 app:app')
+
