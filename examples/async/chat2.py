9f15b835da31549588fc4290d2b12e33d200c4d4
Mark <130257905+kramcat@users.noreply.github.com>
2024-04-10 00:13:43 +0300

    examples

---
+++ examples/async/chat2.py
@@ -0,0 +1,27 @@
+from characterai import aiocai
+import asyncio
+
+async def main():
+    char = input('CHAR ID: ')
+
+    client = aiocai.Client('TOKEN')
+
+    me = await client.get_me()
+
+    async with await client.connect() as chat:
+        new, answer = await chat.new_chat(
+            char, me.id
+        )
+
+        print(f'{answer.name}: {answer.text}')
+        
+        while True:
+            text = input('YOU: ')
+
+            message = await chat.send_message(
+                char, new.chat_id, text
+            )
+
+            print(f'{message.name}: {message.text}')
+
+asyncio.run(main())
